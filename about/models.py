d7bc4a10258491bd5fdefae9909bcb8a127313c8
[lucadm] <[lucadimaio@gmail.com]>
2026-02-24 20:34:48 +0000

    Add about profile image field

--- about/models.py
+++ about/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField #import the CloudinaryField to use for storing images in the cloud
 # Create your models here.
 
 class About(models.Model):
@@ -7,7 +7,7 @@ class About(models.Model):
     title = models.CharField(max_length=200) #title of the about page
     content = models.TextField() #content of the about page
     updated_on = models.DateTimeField(auto_now=True) #date and time when the about page was last updated
-
+    profile_image = CloudinaryField('image', default='placeholder') #profile image for the about page, stored in the cloud using Cloudinary
     def __str__(self):
         return self.title
 
